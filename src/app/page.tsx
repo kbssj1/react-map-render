538807c258320a468e0e70aed8ac8dd01741dd8f
kbssj1 <kbssj1@naver.com>
2024-05-29 22:21:59 +0900

    update transfrom value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,15 @@ let gd = new GeoData(geoData);
 
 export default function Home() {
 
-  const [setScaleX, scaleX] = React.useState<number>(400);
-  const [setScaleY, scaleY] = React.useState<number>(300);
-  const [setTranslateX, translateX] = React.useState<number>(0.2);
-  const [setTranslateY, translateY] = React.useState<number>(-0.3);
+  const [translateX, setTranslateX] = React.useState<number>(400);
+  const [translateY, setTranslateY] = React.useState<number>(300);
+  const [scaleX, setScaleX] = React.useState<number>(0.2);
+  const [scaleY, setScaleY] = React.useState<number>(-0.2);
 
   return (
     <main>
       <svg width='100%' height={3000}>
-        <g
-          transform={`translate(${translateX}, ${translateY}) scale(${scaleX}, ${scaleY})`}>
+        <g transform={`translate(${translateX}, ${translateY}) scale(${scaleX}, ${scaleY})`}>
           {
             gd.getGeoData.map(function(p, index){
               return <path d={p} key={index}> </path>
