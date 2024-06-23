755cc5b96bebeffe93d5eb92244e2245f561b86f
kbssj1 <kbssj1@naver.com>
2024-06-23 22:13:09 +0900

    add temp code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import ManyPoint from "@/lib/ManyPoint";
+import geoData from "../lib/countries.geo";
 
 export default function Home() {
 
   return (
     <main>
-      <ManyPoint width={700} height={700}/>
+      <ManyPoint width={700} height={700} geoData={geoData} />
     </main>
   );
 }
