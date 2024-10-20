a63d3bf1762cad328fd9255687f8b6cd437ba4cf
kbssj1 <kbssj1@naver.com>
2024-10-20 17:25:19 +0900

    ReactMapWebglRender

---
+++ src/lib/ui/ReactMapWebglRender.tsx
@@ -0,0 +1,15 @@
+import WebGl from "../core/webgl/webgl";
+
+interface PropsType {
+  width: number;
+  height: number;
+}
+
+function ReactMapWebglRender(props: PropsType) {
+
+  return (
+    <canvas />
+  );
+}
+
+export default ReactMapWebglRender;
