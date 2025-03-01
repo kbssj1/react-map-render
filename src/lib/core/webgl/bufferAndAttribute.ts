699928ebcc91736bb92c0fec4cc1974e4eead8fc
kbssj1 <kbssj1@naver.com>
2025-03-01 15:15:00 +0900

    add 3d model array

--- src/lib/core/webgl/bufferAndAttribute.ts
+++ src/lib/core/webgl/bufferAndAttribute.ts
@@ -26,7 +26,7 @@ export class BufferAndAttribute {
       // Turn on the attribute
       gl.enableVertexAttribArray(positionAttributeLocation);
       // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-      var size = 2;          // 2 components per iteration
+      var size = 3;          // 2 components per iteration
       var type = gl.FLOAT;   // the data is 32bit floats
       var normalize = false; // don't normalize the data
       var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
@@ -37,46 +37,49 @@ export class BufferAndAttribute {
       var texCoordBuffer = gl.createBuffer();
       gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrays.texcoords), gl.STATIC_DRAW);
-  
-      // Turn on the attribute
-      gl.enableVertexAttribArray(texCoordAttributeLocation);
-      // Tell the attribute how to get data out of texCoordBuffer (ARRAY_BUFFER)
-      var size = 2;          // 2 components per iteration
-      var type = gl.FLOAT;   // the data is 32bit floats
-      var normalize = false; // don't normalize the data
-      var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-      var offset = 0;        // start at the beginning of the buffer
-      gl.vertexAttribPointer(
-          texCoordAttributeLocation, size, type, normalize, stride, offset);
-  
-      // Create a texture.
-      var texture = gl.createTexture();
-  
-      // make unit 0 the active texture uint
-      // (ie, the unit all other texture commands will affect
-      gl.activeTexture(gl.TEXTURE0 + 0);
-  
-      // Bind it to texture unit 0' 2D bind point
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-  
-      // Set the parameters so we don't need mips and so we're not filtering
-      // and we don't repeat at the edges
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  
-      // Upload the image into the texture.
-      var mipLevel = 0;               // the largest mip
-      var internalFormat = gl.RGBA;   // format we want in the texture
-      var srcFormat = gl.RGBA;        // format of data we are supplying
-      var srcType = gl.UNSIGNED_BYTE; // type of data we are supplying
-      gl.texImage2D(gl.TEXTURE_2D,
-                    mipLevel,
-                    internalFormat,
-                    srcFormat,
-                    srcType,
-                    image);
+      
+      if (arrays.useTexture == 1)
+      {
+        // Turn on the attribute
+        gl.enableVertexAttribArray(texCoordAttributeLocation);
+        // Tell the attribute how to get data out of texCoordBuffer (ARRAY_BUFFER)
+        var size = 2;          // 2 components per iteration
+        var type = gl.FLOAT;   // the data is 32bit floats
+        var normalize = false; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            texCoordAttributeLocation, size, type, normalize, stride, offset);
+    
+        // Create a texture.
+        var texture = gl.createTexture();
+    
+        // make unit 0 the active texture uint
+        // (ie, the unit all other texture commands will affect
+        gl.activeTexture(gl.TEXTURE0 + 0);
+    
+        // Bind it to texture unit 0' 2D bind point
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+    
+        // Set the parameters so we don't need mips and so we're not filtering
+        // and we don't repeat at the edges
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    
+        // Upload the image into the texture.
+        var mipLevel = 0;               // the largest mip
+        var internalFormat = gl.RGBA;   // format we want in the texture
+        var srcFormat = gl.RGBA;        // format of data we are supplying
+        var srcType = gl.UNSIGNED_BYTE; // type of data we are supplying
+        gl.texImage2D(gl.TEXTURE_2D,
+                      mipLevel,
+                      internalFormat,
+                      srcFormat,
+                      srcType,
+                      image);
+      }
     }
   }
 
--- src/lib/core/webgl/webgl.ts
+++ src/lib/core/webgl/webgl.ts
@@ -8,7 +8,8 @@ import { Vec3 } from "../math/vec3";
 
 export interface Arrays {
   position: number[],
-  texcoords: number[]
+  texcoords: number[],
+  useTexture: number
 }
 
 /**
@@ -24,14 +25,137 @@ class WebGL {
     this.canvas = canvas;
 
     let array:Arrays = {
-      position : [           
-        0, 0,
-        100, 0,
-        0, 200,
-        0, 200,
-        100, 0,
-        100, 200], 
-      texcoords: [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0,1.0, 0.0, 1.0, 1.0,]
+      position : [
+        // left column front
+          0,   0,  0,
+         30,   0,  0,
+          0, 150,  0,
+          0, 150,  0,
+         30,   0,  0,
+         30, 150,  0,
+
+        // top rung front
+         30,   0,  0,
+        100,   0,  0,
+         30,  30,  0,
+         30,  30,  0,
+        100,   0,  0,
+        100,  30,  0,
+
+        // middle rung front
+         30,  60,  0,
+         67,  60,  0,
+         30,  90,  0,
+         30,  90,  0,
+         67,  60,  0,
+         67,  90,  0,
+
+        // left column back
+          0,   0,  30,
+         30,   0,  30,
+          0, 150,  30,
+          0, 150,  30,
+         30,   0,  30,
+         30, 150,  30,
+
+        // top rung back
+         30,   0,  30,
+        100,   0,  30,
+         30,  30,  30,
+         30,  30,  30,
+        100,   0,  30,
+        100,  30,  30,
+
+        // middle rung back
+         30,  60,  30,
+         67,  60,  30,
+         30,  90,  30,
+         30,  90,  30,
+         67,  60,  30,
+         67,  90,  30,
+
+        // top
+          0,   0,   0,
+        100,   0,   0,
+        100,   0,  30,
+          0,   0,   0,
+        100,   0,  30,
+          0,   0,  30,
+
+        // top rung right
+        100,   0,   0,
+        100,  30,   0,
+        100,  30,  30,
+        100,   0,   0,
+        100,  30,  30,
+        100,   0,  30,
+
+        // under top rung
+        30,   30,   0,
+        30,   30,  30,
+        100,  30,  30,
+        30,   30,   0,
+        100,  30,  30,
+        100,  30,   0,
+
+        // between top rung and middle
+        30,   30,   0,
+        30,   30,  30,
+        30,   60,  30,
+        30,   30,   0,
+        30,   60,  30,
+        30,   60,   0,
+
+        // top of middle rung
+        30,   60,   0,
+        30,   60,  30,
+        67,   60,  30,
+        30,   60,   0,
+        67,   60,  30,
+        67,   60,   0,
+
+        // right of middle rung
+        67,   60,   0,
+        67,   60,  30,
+        67,   90,  30,
+        67,   60,   0,
+        67,   90,  30,
+        67,   90,   0,
+
+        // bottom of middle rung.
+        30,   90,   0,
+        30,   90,  30,
+        67,   90,  30,
+        30,   90,   0,
+        67,   90,  30,
+        67,   90,   0,
+
+        // right of bottom
+        30,   90,   0,
+        30,   90,  30,
+        30,  150,  30,
+        30,   90,   0,
+        30,  150,  30,
+        30,  150,   0,
+
+        // bottom
+        0,   150,   0,
+        0,   150,  30,
+        30,  150,  30,
+        0,   150,   0,
+        30,  150,  30,
+        30,  150,   0,
+
+        // left side
+        0,   0,   0,
+        0,   0,  30,
+        0, 150,  30,
+        0,   0,   0,
+        0, 150,  30,
+        0, 150,   0,
+      ], 
+      texcoords: [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0,1.0, 0.0, 1.0, 1.0,],
+      useTexture: 0
     };
     
     let vertexShader = new Shader(this.gl, this.gl.VERTEX_SHADER, VERTEX_SHADER);
@@ -67,18 +191,18 @@ class WebGL {
 
     var matrix:Mat4 = new Mat4([2 / this.canvas.clientWidth, 0, 0, 0, 0, -2 / this.canvas.clientHeight, 0, 0, 0, 0, 2 / 400, 0, -1, 1, 0, 1]);
     matrix.translate(new Vec3([100, 50, 0]));
-    matrix.scale(new Vec3([2, 1, 1]));
-    // matrix.rotate(Math.PI * 0.1, new Vec3([0, 0, 1]));
+    matrix.scale(new Vec3([1, 1, 1]));
+    matrix.rotate(Math.PI * 0.1, new Vec3([1, 1, 1]));
 
     // uniform
     gl.uniform1i(imageLocation, 0);
     gl.uniformMatrix4fv(matrixLocation, false, matrix.array());
-    gl.uniform1i(useTextureLocation, 1); // 텍스처를 사용하지 않고 단색 출력
+    gl.uniform1i(useTextureLocation, array.useTexture); // 텍스처를 사용하지 않고 단색 출력
 
     // Draw the rectangle.
     var primitiveType = gl.TRIANGLES;
     var offset = 0; 
-    var count = 6;
+    var count = array.position.length / 3;
     gl.drawArrays(primitiveType, offset, count);
   }
 
