88ef4626fcd40c112e7058629580f9eb171c915d
kbssj1 <kbssj1@naver.com>
2025-10-25 11:07:39 +0900

    add setIdentity func

--- src/lib/core/math/mat4.ts
+++ src/lib/core/math/mat4.ts
@@ -282,6 +282,27 @@ export class Mat4 {
     return this
   } 
 
+  setIdentity(): Mat4 {
+    this.values[0] = 1
+    this.values[1] = 0
+    this.values[2] = 0
+    this.values[3] = 0
+    this.values[4] = 0
+    this.values[5] = 1
+    this.values[6] = 0
+    this.values[7] = 0
+    this.values[8] = 0
+    this.values[9] = 0
+    this.values[10] = 1
+    this.values[11] = 0
+    this.values[12] = 0
+    this.values[13] = 0
+    this.values[14] = 0
+    this.values[15] = 1
+
+    return this;
+  }
+
   static lookAt(position: Vec3, target: Vec3, up: Vec3 = Vec3.up): Mat4 {
     if (position.equals(target)) {
         return this.identity
